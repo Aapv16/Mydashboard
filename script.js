6b7ab94e6df1245ffa172e890e1eb7ace504a543
Aapv16 <afrianusvicky38@gmail.com>
2024-12-27 23:29:46 +0700

    tambah kelas active

--- script.js
+++ script.js
@@ -10,5 +10,18 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("navbar-light");
   }
 });
+// .......................
+// Ambil semua elemen dengan kelas "nav-link" dan simpan dalam variabel ubahWarna
+const ubahWarna = document.querySelectorAll(".nav-link");
 
-// kasus
+// Iterasi melalui setiap elemen "nav-link"
+ubahWarna.forEach((link) => {
+  // Tambahkan event listener untuk menangkap event "click" pada elemen
+  link.addEventListener("click", function (event) {
+    // Loop melalui semua elemen "nav-link" untuk menghapus kelas "active"
+    ubahWarna.forEach((nav) => nav.classList.remove("active"));
+
+    // Tambahkan kelas "active" ke elemen yang diklik
+    this.classList.add("active");
+  });
+});
